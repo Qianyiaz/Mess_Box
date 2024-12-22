f1f87ec071cdf425aa1109f7a5be3aa5ee70da08
千依 <Qusay_Diaz.github@outlook.com>
2024-12-22 16:01:33 +0800

    初始化仓库。

---
+++ Code/MainClass.cs
@@ -0,0 +1,15 @@
+﻿using NeoModLoader.api;
+
+namespace Mess_Box.Code
+{
+    public class MainClass : BasicMod<MainClass>
+    {
+        protected override void OnModLoad()
+        {
+            // Load your mod here
+            // 加载你的mod内容
+            // LogInfo(GetConfig()["Default"]["WhatToSay"].TextVal); // Call this only then you confirm it is a text config item
+            LogInfo(GetConfig()["Default"]["WhatToSay"].GetValue() as string);
+        }
+    }
+}
